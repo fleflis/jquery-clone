dc7d3f74455f7afeec82d400c9efca9b42d587e6
Christian Sanches <christian@endless-point.org>
2020-08-31 09:33:53 -0300

    .css prop

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const $ = (...args) => {
     // Document.ready function
     const readyFn = args[0];
     document.addEventListener('DOMContentLoaded', readyFn);
+  } else if (typeof args[0] === 'string') {
+    // QuerySelector
+    const query = args[0];
+
+    const collection = document.querySelectorAll(query);
+    collection.css = (...cssArgs) => {
+      if (typeof cssArgs[0] === 'string') {
+        const [property, value] = cssArgs;
+        collection.forEach((element) => {
+          element.style[property] = value;
+        })
+      } else if (typeof cssArgs[0] === 'object') {
+        const cssProps = Object.entries(cssArgs[0]);
+        collection.forEach((element) => {
+          cssProps.forEach(([property, value]) => {
+            element.style[property] = value;
+          })
+        })
+      }
+    }
+
+    return collection
 
   }
 }
